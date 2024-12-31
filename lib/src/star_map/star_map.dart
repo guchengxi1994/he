d57e490f2aa39bb2b0fd6822f72eb276faa3d64a
xiaoshuyui <528490652@qq.com>
2024-12-31 22:39:17 +0800

    Update star_map.dart

--- lib/src/star_map/star_map.dart
+++ lib/src/star_map/star_map.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 const double _singleMapWidth = 300;
@@ -18,9 +20,35 @@ class Constellation {
   Constellation(this.name, this.points, this.lines);
 }
 
+class Star {
+  Offset position;
+  double radius;
+  double opacity;
+
+  Star({required this.position, required this.radius, required this.opacity});
+
+  factory Star.random(double width) {
+    final random = Random();
+    return Star(
+      position: Offset(
+          random.nextDouble() * (_mapWidth + width) - _mapWidth / 2,
+          random.nextDouble() * (_mapHeight - 100) +
+              50), // Replace with your screen size
+      radius: random.nextDouble() * 2 + 1,
+      opacity: random.nextDouble(),
+    );
+  }
+
+  void update() {
+    final random = Random();
+    opacity = 0.5 + 0.5 * random.nextDouble(); // Randomize opacity for blinking
+  }
+}
+
 class StarMapPainter extends CustomPainter {
   final double viewWindowWidth;
-  StarMapPainter({this.viewWindowWidth = 400});
+  StarMapPainter({this.viewWindowWidth = 400, required this.stars});
+  final List<Star> stars;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -44,6 +72,16 @@ class StarMapPainter extends CustomPainter {
     List<Constellation> constellations = [
       _buildAries(),
       _buildTaurus(),
+      _buildGemini(),
+      _buildCancer(),
+      _buildLeo(),
+      _buildVirgo(),
+      _buildLibra(),
+      _buildScorpio(),
+      _buildSagittarius(),
+      _buildCapricorn(),
+      _buildAquarius(),
+      _buildPisces()
     ];
     for (int i = 0; i < constellations.length; i++) {
       canvas.restore();
@@ -52,6 +90,42 @@ class StarMapPainter extends CustomPainter {
       _paintConstellation(canvas, constellations[i]);
     }
     canvas.restore();
+
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1;
+    for (var star in stars) {
+      paint.color = Colors.white.withValues(alpha: star.opacity);
+      canvas.drawCircle(star.position, star.radius, paint);
+    }
+
+    // Draw meteors
+    // for (var meteor in meteors) {
+    //   final meteorPaint = Paint()
+    //     ..shader = LinearGradient(
+    //       colors: [
+    //         Colors.white.withValues(alpha: meteor.opacity),
+    //         Colors.blue.withValues(alpha: meteor.opacity * 0.5),
+    //       ],
+    //       begin: Alignment.center,
+    //       end: Alignment.topLeft,
+    //     ).createShader(
+    //         Rect.fromLTWH(meteor.position.dx, meteor.position.dy, 10, 10))
+    //     ..style = PaintingStyle.fill;
+
+    //   canvas.drawCircle(meteor.position, 2.0, meteorPaint);
+
+    //   // Meteor trail
+    //   final trailPaint = Paint()
+    //     ..color = Colors.white.withValues(alpha: meteor.opacity * 0.2)
+    //     ..strokeWidth = 2.0
+    //     ..style = PaintingStyle.stroke;
+    //   canvas.drawLine(
+    //     meteor.position,
+    //     meteor.position - meteor.velocity * 5, // Extend trail backward
+    //     trailPaint,
+    //   );
+    // }
   }
 
   void _paintConstellation(Canvas canvas, Constellation constellation) {
@@ -118,12 +192,14 @@ class StarMapPainter extends CustomPainter {
       Offset(50, 50),
       Offset(200, 100),
       Offset(230, 140),
-      Offset(235, 160),
+      Offset(255, 160),
+      Offset(25, 70),
     ];
     List<List<int>> lines = [
       [0, 1],
       [1, 2],
       [2, 3],
+      [4, 0],
     ];
     return Constellation('ARIES', points.addOffset(Offset(0, 50)), lines);
   }
@@ -166,30 +242,438 @@ class StarMapPainter extends CustomPainter {
         "Taurus".toUpperCase(), points.addOffset(Offset(0, 50)), lines);
   }
 
+  Constellation _buildGemini() {
+    // 12 points
+    List<Offset> points = [
+      /*0*/ Offset(80, 80),
+      /*1*/ Offset(50, 110),
+      /*2*/ Offset(75, 140),
+      /*3*/ Offset(110, 160),
+      /*4*/ Offset(130, 180),
+      /*5*/ Offset(170, 200), // connection point1
+      /*6*/ Offset(190, 180),
+      /*7*/ Offset(210, 160), // connection point2
+      /*8*/ Offset(115, 100),
+      /*9*/ Offset(100, 90),
+      /*10*/ Offset(160, 220),
+      /*11*/ Offset(230, 155),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [8, 0],
+      [5, 10],
+      [7, 11]
+    ];
+
+    return Constellation("Gemini".toUpperCase(), points, lines);
+  }
+
+  Constellation _buildCancer() {
+    // 5 points
+    List<Offset> points = [
+      /*0*/ Offset(150, 30),
+      /*1*/ Offset(170, 100),
+      /*2*/ Offset(160, 120),
+      /*3*/ Offset(140, 160),
+      /*4*/ Offset(190, 170),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [2, 4],
+    ];
+
+    return Constellation(
+        "Cancer".toUpperCase(), points.addOffset(Offset(0, 59)), lines);
+  }
+
+  Constellation _buildLeo() {
+    // 9 points
+    List<Offset> points = [
+      /*0*/ Offset(250, 60),
+      /*1*/ Offset(230, 50),
+      /*2*/ Offset(190, 65),
+      /*3*/ Offset(180, 100), // connection point1
+      /*4*/ Offset(210, 120),
+      /*5*/ Offset(205, 160),
+      /*6*/ Offset(100, 140),
+      /*7*/ Offset(30, 160),
+      /*8*/ Offset(90, 100),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [3, 8]
+    ];
+
+    return Constellation(
+        "Leo".toUpperCase(), points.addOffset(Offset(0, 50)), lines);
+  }
+
+  Constellation _buildVirgo() {
+    // 12 points
+    List<Offset> points = [
+      /*0*/ Offset(50, 50),
+      /*1*/ Offset(40, 80),
+      /*2*/ Offset(80, 90), // connection point1
+      /*3*/ Offset(120, 80), // connection point2
+      /*4*/ Offset(140, 60),
+      /*5*/ Offset(160, 40),
+      /*6*/ Offset(115, 120), // connection point3
+      /*7*/ Offset(130, 160),
+      /*8*/ Offset(80, 190),
+      /*9*/ Offset(90, 130), // connection point4
+      /*10*/ Offset(70, 160),
+      /*11*/ Offset(30, 140),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      //
+      [3, 6],
+      [6, 7],
+      [7, 8],
+      //
+      [6, 9],
+      [9, 2],
+      [9, 10],
+      [10, 11],
+    ];
+
+    return Constellation(
+        "Virgo".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
+  Constellation _buildLibra() {
+    // 6 points
+    List<Offset> points = [
+      /*0*/ Offset(80, 50),
+      /*1*/ Offset(50, 80),
+      /*2*/ Offset(130, 90),
+      /*3*/ Offset(40, 140),
+      /*4*/ Offset(35, 150),
+      /*5*/ Offset(110, 130),
+    ];
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 0],
+      //
+      [2, 5],
+      [1, 3],
+      [3, 4],
+    ];
+    return Constellation(
+        "Libra".toUpperCase(), points.addOffset(Offset(80, 50)), lines);
+  }
+
+  Constellation _buildScorpio() {
+    // 13 points
+    List<Offset> points = [
+      /*0*/ Offset(50, 130),
+      /*1*/ Offset(70, 120),
+      /*2*/ Offset(80, 125),
+      /*3*/ Offset(60, 150),
+      /*4*/ Offset(80, 170),
+      /*5*/ Offset(120, 160),
+      /*6*/ Offset(125, 120),
+      /*7*/ Offset(130, 110),
+      /*8*/ Offset(160, 80),
+      /*9*/ Offset(170, 70),
+      /*10*/ Offset(175, 65),
+      /*11*/ Offset(200, 60), // connection point1
+      /*12*/ Offset(180, 40),
+      /*13*/ Offset(190, 75),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [8, 9],
+      [9, 10],
+      [10, 11],
+      [11, 12],
+      [11, 13]
+    ];
+
+    return Constellation(
+        "Scorpio".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
+  Constellation _buildSagittarius() {
+    // 19 points
+    List<Offset> points = [
+      /*0*/ Offset(100, 200),
+      /*1*/ Offset(100, 180),
+      /*2*/ Offset(70, 190), // connection point1
+      /*3*/ Offset(60, 170),
+      /*4*/ Offset(50, 160),
+      /*5*/ Offset(50, 130),
+      /*6*/ Offset(80, 110),
+      /*7*/ Offset(100, 135), // connection point2
+      /*8*/ Offset(110, 145),
+      /*9*/ Offset(125, 135), // connection point3
+      /*10*/ Offset(115, 120),
+      /*11*/ Offset(110, 100),
+      /*12*/ Offset(100, 90),
+      /*13*/ Offset(145, 125), // connection point4
+      /*14*/ Offset(160, 110),
+      /*15*/ Offset(140, 145),
+      /*16*/ Offset(120, 165),
+      /*17*/ Offset(130, 180),
+      /*18*/ Offset(155, 155),
+    ];
+    List<List<int>> lines = [
+      [0, 2],
+      [1, 2],
+      //
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      //
+      [7, 8],
+      [8, 9],
+      [9, 10],
+      [7, 10],
+      //
+      [10, 11],
+      [11, 12],
+      //
+      [9, 13],
+      [13, 14],
+      [13, 15],
+      [15, 16],
+      [16, 17],
+      [15, 18]
+    ];
+
+    return Constellation(
+        "Sagittarius".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
+  Constellation _buildCapricorn() {
+    // 11 points
+    List<Offset> points = [
+      /*0*/ Offset(50, 50),
+      /*1*/ Offset(60, 55),
+      /*2*/ Offset(75, 60),
+      /*3*/ Offset(100, 70),
+      /*4*/ Offset(150, 40), // connection point1
+      /*5*/ Offset(135, 70),
+      /*6*/ Offset(105, 110),
+      /*7*/ Offset(100, 105),
+      /*8*/ Offset(80, 95),
+      /*9*/ Offset(65, 80),
+      /*10*/ Offset(160, 30),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [8, 9],
+      [9, 0],
+      [4, 10],
+    ];
+
+    return Constellation(
+        "Capricorn".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
+  Constellation _buildAquarius() {
+    // 13 points
+    List<Offset> points = [
+      /*0*/ Offset(50, 120),
+      /*1*/ Offset(70, 90),
+      /*2*/ Offset(65, 80),
+      /*3*/ Offset(40, 60),
+      /*4*/ Offset(100, 40),
+      /*5*/ Offset(110, 50),
+      /*6*/ Offset(120, 60),
+      /*7*/ Offset(130, 50), // connection point1
+      /*8*/ Offset(160, 80),
+      /*9*/ Offset(180, 90),
+      /*10*/ Offset(120, 70),
+      /*11*/ Offset(130, 80),
+      /*12*/ Offset(135, 85),
+    ];
+
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [8, 9],
+      [7, 10],
+      [10, 11],
+      [11, 12]
+    ];
+
+    return Constellation(
+        "Aquarius".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
+  Constellation _buildPisces() {
+    // 14 points
+    List<Offset> points = [
+      /*0*/ Offset(50, 50),
+      /*1*/ Offset(40, 60),
+      /*2*/ Offset(55, 70),
+      /*3*/ Offset(65, 100),
+      /*4*/ Offset(60, 130),
+      /*5*/ Offset(45, 160),
+      /*6*/ Offset(65, 140),
+      /*7*/ Offset(85, 130),
+      /*8*/ Offset(100, 110),
+      /*9*/ Offset(120, 100), // connection point1
+      /*10*/ Offset(130, 120),
+      /*11*/ Offset(145, 130),
+      /*12*/ Offset(160, 115),
+      /*13*/ Offset(140, 100),
+    ];
+    List<List<int>> lines = [
+      [0, 1],
+      [1, 2],
+      [0, 2],
+      //
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [8, 9],
+      [9, 10],
+      [10, 11],
+      [11, 12],
+      [12, 13],
+      [13, 9]
+    ];
+    return Constellation(
+        "Pisces".toUpperCase(), points.addOffset(Offset(30, 50)), lines);
+  }
+
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return false;
   }
 }
 
-class StarMap extends StatelessWidget {
+class StarMap extends StatefulWidget {
   const StarMap({super.key, required this.viewWindowWidth});
   final double viewWindowWidth;
 
   @override
+  State<StarMap> createState() => _StarMapState();
+}
+
+class _StarMapState extends State<StarMap> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late List<Star> _stars;
+  final TransformationController _transformationController =
+      TransformationController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // _transformationController.addListener(() {
+    //   final Offset offset = Offset(
+    //       _transformationController.value.getTranslation().x,
+    //       _transformationController.value.getTranslation().y);
+
+    //   debugPrint('当前移动距离：$offset');
+    // });
+
+    // Initialize stars
+    _stars = List.generate(200, (_) => Star.random(widget.viewWindowWidth));
+
+    // Animation controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )
+      ..addListener(() {
+        setState(() {
+          // Update star opacity to simulate blinking
+          for (var star in _stars) {
+            star.update();
+          }
+        });
+      })
+      ..repeat(reverse: true);
+
+    Future.delayed(Duration.zero, _generateMeteor);
+  }
+
+  // TODO: Generate meteors
+  void _generateMeteor() async {}
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _transformationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final double v = (_mapWidth - viewWindowWidth) / 2;
+    final double v = (_mapWidth - widget.viewWindowWidth) / 2;
     return SizedBox(
-      width: viewWindowWidth,
+      width: widget.viewWindowWidth,
       height: _mapHeight,
       child: InteractiveViewer(
+          transformationController: _transformationController,
+          maxScale: 1,
+          minScale: 1,
           boundaryMargin: EdgeInsets.only(left: v, right: v),
           child: SizedBox(
             width: _mapWidth,
             height: _mapHeight,
             child: CustomPaint(
               size: Size(_mapWidth, _mapHeight),
-              painter: StarMapPainter(viewWindowWidth: viewWindowWidth),
+              painter: StarMapPainter(
+                viewWindowWidth: widget.viewWindowWidth,
+                stars: _stars,
+              ),
             ),
           )),
     );
