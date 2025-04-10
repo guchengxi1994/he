5d476337b029693dbdfee46fe4200e1bd862abaa
guchengxi1994 <528490652@qq.com>
2025-04-10 04:13:17 +0000

    Deploying to gh-pages from @ guchengxi1994/he@7f584d1195f2debb6970211c881ce1d25ee053af 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4053570195"
+    serviceWorkerVersion: "2958410117"
   }
 });
